98f234a2f07fa82f31bdd916b03a8423925a3346
pepa <mogwais@outlook.com>
2021-01-21 16:45:26 +0000

    Add TS interface and pass prop

--- demo-ts/src/App.tsx
+++ demo-ts/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header buttonText="View"/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ demo-ts/src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface HeaderProps {
+    buttonText: string;
+}
+
+export default function Header(props: HeaderProps) {
+    return (
+        <div>
+            <h1>Header</h1>
+            <button>{props.buttonText }</button>
+        </div>
+    )
+}
