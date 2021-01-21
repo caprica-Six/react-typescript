200e6a4f0720796b8b2c821e0fd78c005b48efce
pepa <mogwais@outlook.com>
2021-01-21 17:03:36 +0000

    Add Hooks counter

--- demo-ts/src/App.tsx
+++ demo-ts/src/App.tsx
@@ -6,21 +6,14 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <Header buttonText="View"/>
+      <Header buttonText="Count"/>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
--- demo-ts/src/Header.tsx
+++ demo-ts/src/Header.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 interface HeaderProps {
     buttonText: string;
 }
 
-export default function Header({ buttonText}: HeaderProps) {
+export default function Header({ buttonText }: HeaderProps) {
+       const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div>
             <h1>Header</h1>
-            <button>{buttonText }</button>
+            <button onClick={increment}>{buttonText}</button>
+            
+            <p>{ count}</p>
         </div>
     )
 }
