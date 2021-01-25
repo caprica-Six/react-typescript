056a950dbc83a190da06afe11051c14fc97abeab
pepa <mogwais@outlook.com>
2021-01-25 16:38:27 +0000

    Remove unused

--- demo-ts/src/App.tsx
+++ demo-ts/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 
@@ -9,7 +8,6 @@ function App() {
       <Header />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
